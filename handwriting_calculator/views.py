f829d394119bfbddabb89e6187cf1ac8e6b8cda0
hitzx <1286052098@qq.com>
2018-07-14 14:06:11 +0800

    improve the model's accuracy

--- handwriting_calculator/views.py
+++ handwriting_calculator/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.templatetags.static import static
+
+meta = static("./model/model-200.meta")
+path = static("./model/")
+
+from django.conf import settings
 import numpy as np
 from .utils.cnn_model import *
 from .utils.image_processing import *
 from .utils.calculator import *
-
-cnn_model = model()
-cnn_model.load_model('', '')
+import os
 
 
 def main_page(request):
@@ -17,9 +21,17 @@ def main_page(request):
 @csrf_exempt
 def get_result(request):
     img_str = request.POST["img_data"]
-    img_arr = np.array(img_str.split(',')).reshape(4, 1000, 200)
-    img = (img_arr[0] + img_arr[1] + img_arr[2]) / 3
-    image_cuts = get_image_cuts(img, data_needed=True)
+
+    print(os.path.abspath(meta))
+    cnn_model = model()
+    cnn_model.load_model(meta, path)
+    img_arr = np.array(img_str.split(',')).reshape(4, 1000, 200).astype(np.float)
+    # img = (img_arr[0] + img_arr[1] + img_arr[2]) / 3
+    img = img_arr[0]
+    print(img)
+    img = img.T
+    cv2.imwrite('img.jpg', img)
+    image_cuts = get_image_cuts(img, is_data=True, data_needed=True)
     equation = ''
     for image in image_cuts:
         predict = cnn_model.predict(image)
